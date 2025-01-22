bcd54daea9de97615a9806b9ee3e8b33d99f839d
noel-create <ladi.noel20@gmail.com>
2025-01-22 20:58:40 +0100

    Update launch.py

--- launch.py
+++ launch.py
@@ -9,17 +9,14 @@ required_packages = [
     'keyboard', 'python-dateutil', 'pywin32', 'zipfile'
 ]
 
-# Function to install packages
 def install_packages():
     try:
-        # Loop through and install each package using pip
         for package in required_packages:
             subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
         print("All packages installed successfully!")
     except subprocess.CalledProcessError as e:
         print(f"Failed to install {e.cmd}. Error: {e}")
 
-# Call the function to install packages
 install_packages()
 
 time.sleep(3)
